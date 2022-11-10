ede75b28df25f75f370fce7137c9c97d373f8fcc
B6216023 <89638172+Kassarapron@users.noreply.github.com>
2022-11-10 13:19:19 +0700

    add fronend show product

--- fontend/src/app/components/adminMode/manageproduct/manageproduct.component.ts
+++ fontend/src/app/components/adminMode/manageproduct/manageproduct.component.ts
@@ -1,15 +1,34 @@
 import { Component, OnInit } from '@angular/core';
+import { BookService } from 'src/app/services/book.service';
 
 @Component({
   selector: 'app-manageproduct',
   templateUrl: './manageproduct.component.html',
   styleUrls: ['./manageproduct.component.css']
 })
+
 export class ManageproductComponent implements OnInit {
+  products: any
 
-  constructor() { }
-
-  ngOnInit(): void {
+  ngOnInit(): void{
   }
 
+  constructor(private BookService: BookService) {
+    this.onLoading();
+  }
+
+  onLoading(){
+    try{
+      this.BookService.getProducts().subscribe(
+        {next : data =>{
+          console.log(data)
+          this.products = data;
+        },
+        error : err =>{
+          console.log(err)
+        }});
+    }catch(error){
+      console.log(error)
+    }
+  }
 }
